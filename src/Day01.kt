56019e27106e6186f2db7424165ed4bdb5fb29c3
Brian Piejak <bpiejak@yext.com>
2021-12-10 14:08:51 -0500

    Cleanup with int mapping as inputs are read

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,9 +1,8 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Int>): Int {
         var count = 0
-        var prev = input[0].toInt()
-        for (i in input) {
-            val current = i.toInt()
+        var prev = input[0]
+        for (current in input) {
             if (current > prev) {
                 count++
             }
@@ -12,27 +11,27 @@ fun main() {
         return count
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Int>): Int {
         var count = 0
-        var prev = input[0].toInt()
+        var prev = input[0]
         for (i in input.indices) {
             if (i > 2) {
-                val current = input[i].toInt()
+                val current = input[i]
                 if (current > prev) {
                     count++
                 }
-                prev = input[i - 2].toInt()
+                prev = input[i - 2]
             }
         }
         return count
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputAsInt("Day01_test")
     check(part1(testInput) == 7)
     check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputAsInt("Day01")
     println(part1(input))
     println(part2(input))
 }
